64e209421bef048202068007fa7a6c28f9bd0cf0
vvvar <voinovvladv@gmail.com>
2024-01-21 23:17:23 +0100

    MD019: Better problem area highlight

--- src/checkmark_lint/src/md019_multiple_spaces_after_hash_in_atx_heading.rs
+++ src/checkmark_lint/src/md019_multiple_spaces_after_hash_in_atx_heading.rs
@@ -1,5 +1,6 @@
 use crate::violation::{Violation, ViolationBuilder};
-use common::{find_offset_by_line_number, MarkDownFile};
+use common::{for_each, parse, MarkDownFile};
+use markdown::mdast::{Heading, Node};
 use regex::Regex;
 
 fn violation_builder() -> ViolationBuilder {
@@ -15,37 +16,32 @@ fn violation_builder() -> ViolationBuilder {
 // with atx heading with more then
 // one space
 // Example: "##   this_will_return_true"
-fn start_with_atx_heading_without_space(text: &str) -> bool {
+fn start_with_atx_heading_without_space(h: &Heading, source: &str) -> bool {
+    let line = h.position.as_ref().unwrap().start.line;
+    let text = source.lines().nth(line - 1).unwrap_or("");
     // Pattern: start of the line followed by one or more hash
     //          characters followed by single space and one or
     //          more spaces followed by non-numeric letter
     Regex::new(r"^#+\s\s+\b").unwrap().is_match(text)
 }
 
-fn to_issue(line_number: usize, line: &str, file: &str) -> Violation {
-    let offset_start = find_offset_by_line_number(file, line_number);
-    let offset_end = offset_start + line.find(' ').unwrap_or(0) + 1; // + 1 whitespace
-    violation_builder()
-        .position(&Some(markdown::unist::Position::new(
-            line_number,
-            1,
-            offset_start,
-            line_number,
-            1,
-            offset_end,
-        )))
-        .build()
-}
-
 pub fn md019_multiple_spaces_after_hash_on_atx_style_heading(
     file: &MarkDownFile,
 ) -> Vec<Violation> {
     log::debug!("[MD019] File: {:#?}", &file.path);
-    file.content
-        .lines()
-        .enumerate()
-        .filter(|(_, line)| start_with_atx_heading_without_space(line))
-        .map(|(i, line)| to_issue(i, line, &file.content))
+
+    let ast = parse(&file.content).unwrap();
+    let mut headings: Vec<&Heading> = vec![];
+    for_each(&ast, |node| {
+        if let Node::Heading(h) = node {
+            headings.push(h);
+        }
+    });
+
+    headings
+        .iter()
+        .filter(|h| start_with_atx_heading_without_space(&h, &file.content))
+        .map(|h| violation_builder().position(&h.position).build())
         .collect()
 }
 
@@ -64,7 +60,7 @@ mod tests {
 
         assert_eq!(
             vec![violation_builder()
-                .position(&Some(markdown::unist::Position::new(0, 1, 0, 0, 1, 2)))
+                .position(&Some(markdown::unist::Position::new(1, 1, 0, 1, 8, 7)))
                 .build(),],
             md019_multiple_spaces_after_hash_on_atx_style_heading(&file)
         );
