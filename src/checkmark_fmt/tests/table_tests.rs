2283b201d5cdd1d13fc9fdeecc027b8d312bd59a
vvvar <voinovvladv@gmail.com>
2024-01-23 02:44:32 +0100

    Additional test case for table; Bump minor version

--- src/checkmark_fmt/tests/table_tests.rs
+++ src/checkmark_fmt/tests/table_tests.rs
@@ -1,22 +1,37 @@
 mod utils;
 
-/// Tables
+/// Tables is automatically aligned
 #[test]
-fn table() {
+fn table_auto_aligned() {
     utils::assert_changed_after_formatting(
         "# Table
 
 | Syntax | Description | Something Else | Opapa [Here](./Cargo.lock) |
 | :-- | -----: | :--: | ---- |
-| Header | Title | fdfdsf | On ho! |
+| Header | Title | text | On ho! |
 | Paragraph | Text | dfdfdfdfdfdfdfdfdfdfdfdfdfddsf | Lol |
 ",
         "# Table
 
 | Syntax    | Description | Something Else                 | Opapa [Here](./Cargo.lock) |
 | :-------- | ----------: | :----------------------------: | -------------------------- |
-| Header    | Title       | fdfdsf                         | On ho!                     |
+| Header    | Title       | text                           | On ho!                     |
 | Paragraph | Text        | dfdfdfdfdfdfdfdfdfdfdfdfdfddsf | Lol                        |
+",
+    );
+}
+
+#[test]
+fn table_with_special_symbols() {
+    // Here second row looks like miss-aligned because two "\\"
+    // are just to escape the first one
+    utils::assert_unchanged_after_formatting(
+        "#Table
+
+| Syntax | Description |
+| ------ | ----------: |
+| Header | Title       |
+| -\\>    | Text        |
 ",
     );
 }
