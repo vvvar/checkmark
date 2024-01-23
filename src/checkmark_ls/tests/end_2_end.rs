f674a2317694bdb1fdafb944a71144e2e22922ca
vvvar <voinovvladv@gmail.com>
2024-01-23 03:08:44 +0100

    Fix ls integration tests

--- src/checkmark_ls/tests/end_2_end.rs
+++ src/checkmark_ls/tests/end_2_end.rs
@@ -9,6 +9,7 @@ fn activate_logging() {
 #[ignore = "Involves real HTTP req - unstable. Use manual invocation and verification."]
 #[tokio::test]
 async fn ls() {
-    let files = checkmark_ls::ls("https://github.com/google/googletest.git", &vec![]).await;
+    let tui = common::tui::CheckProgressTUI::new_thread_safe(true);
+    let files = checkmark_ls::ls("https://github.com/google/googletest.git", &vec![], &tui).await;
     assert_eq!(files.len(), 27); // There are some files in the repo
 }
