34d5a1dc7d83b592728edf869de47e16a29a6968
vvvar <voinovvladv@gmail.com>
2024-01-24 15:47:55 +0100

    Additional logging

--- src/checkmark_render/src/lib.rs
+++ src/checkmark_render/src/lib.rs
@@ -8,6 +8,7 @@ use std::fs::{create_dir_all, remove_dir_all, write};
 use std::path::PathBuf;
 use std::str::FromStr;
 use std::string::String;
+use log::debug;
 
 /// Collect links from file
 pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<String> {
@@ -47,15 +48,26 @@ pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<String> 
 }
 
 fn copy_associated_files(files: &Vec<String>, output_dir: &str) {
+    debug!("Copying associated files: {:#?}", &files);
+
     let cwd = current_dir().unwrap();
+    debug!("Current working directory: {:#?}", &cwd);
 
     for file in files {
-        let relative_file_path = file
-            .strip_prefix(&cwd.clone().into_os_string().to_str().unwrap())
-            .unwrap();
+        debug!("Copying associated file: {:#?}", &file);
+
+        let cwd_str = cwd.clone().into_os_string().to_str().unwrap().to_string();
+        debug!("Current working directory srt: {:#?}", &cwd_str);
+
+        let relative_file_path = file.strip_prefix(&cwd_str).unwrap();
+        debug!("Relative file path: {:#?}", &relative_file_path);
+
         let output_file_path = format!("{}{}", &output_dir, &relative_file_path);
+        debug!("Output file path: {:#?}", &output_file_path);
+
         let pb = PathBuf::from_str(&output_file_path).unwrap();
         let output_file_parent_dir_path = pb.parent().unwrap();
+        debug!("Output file parent dir path: {:#?}", &output_file_parent_dir_path);
         // if let Ok(_) = remove_dir_all(&output_file_parent_dir_path) {}
         if let Ok(_) = create_dir_all(&output_file_parent_dir_path) {}
         std::fs::copy(&file, &output_file_path).unwrap();
@@ -69,10 +81,12 @@ pub async fn render(files: &Vec<MarkDownFile>) {
         .to_str()
         .unwrap()
         .to_string();
+    debug!("Rendering to {}", &output_dir);
     remove_dir_all(&output_dir).unwrap();
     create_dir_all(&output_dir).unwrap();
 
     let associated_files = collect_associated_files(&files).await;
+    debug!("Associated files: {:#?}", &associated_files);
     copy_associated_files(&associated_files, &output_dir);
 
     for file in files {
@@ -84,7 +98,11 @@ pub async fn render(files: &Vec<MarkDownFile>) {
             .unwrap()
             .strip_suffix(".md")
             .unwrap();
+        debug!("Relative file path: {:#?}", &relative_file_path);
+
         let output_file_path = format!("{}{}.html", &output_dir, &relative_file_path);
+        debug!("Output file path: {:#?}", &output_file_path);
+
         create_dir_all(&PathBuf::from_str(&output_file_path).unwrap().parent().unwrap()).unwrap();
         write(&output_file_path, &html).unwrap();
     }
