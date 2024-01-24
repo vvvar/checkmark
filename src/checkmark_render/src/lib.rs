9ed76fa76de3ef44b639c2494daa527fb269bc5f
vvvar <voinovvladv@gmail.com>
2024-01-24 22:10:17 +0100

    Use PathBuf instead of raw str concat

--- src/checkmark_render/src/lib.rs
+++ src/checkmark_render/src/lib.rs
@@ -7,12 +7,11 @@ use regex::Regex;
 use std::collections::HashSet;
 use std::env::current_dir;
 use std::fs::{create_dir_all, remove_dir_all, write};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 use std::str::FromStr;
-use std::string::String;
 
 /// Collect links from file
-pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<String> {
+pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<PathBuf> {
     let input = files
         .iter()
         .map(|file| Input {
@@ -34,7 +33,6 @@ pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<String> 
     for link in links {
         links_set.insert(link.clone());
     }
-    debug!("De-duplicated links:\n{:#?}", &links_set);
     links_set
         .into_iter()
         .filter(|link| {
@@ -44,87 +42,48 @@ pub async fn collect_associated_files(files: &Vec<MarkDownFile>) -> Vec<String> 
         })
         .map(|link| {
             let mut uri = link.uri.as_str().to_string();
-            debug!("Associated file uri: {:#?}", &uri);
             uri = match uri.strip_prefix("file://") {
                 Some(stripped_uri) => stripped_uri.to_string(),
                 None => uri,
             };
-            debug!("Associated file uri stripped: {:#?}", &uri);
-            dunce::canonicalize(&uri)
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string()
+            PathBuf::from_str(&uri).unwrap().canonicalize().unwrap()
         })
         .collect()
 }
 
-fn copy_associated_files(files: &Vec<String>, output_dir: &str) {
-    debug!("Copying associated files: {:#?}", &files);
-
+fn copy_associated_files(files: &Vec<PathBuf>, output_dir: &PathBuf) {
     let cwd = current_dir().unwrap();
-    debug!("Current working directory: {:#?}", &cwd);
-
     for file in files {
-        debug!("Copying associated file: {:#?}", &file);
-
-        let cwd_str = cwd.clone().into_os_string().to_str().unwrap().to_string();
-        debug!("Current working directory srt: {:#?}", &cwd_str);
-
-        let relative_file_path = file.strip_prefix(&cwd_str).unwrap();
-        debug!("Relative file path: {:#?}", &relative_file_path);
-
-        let output_file_path = format!("{}{}", &output_dir, &relative_file_path);
-        debug!("Output file path: {:#?}", &output_file_path);
-
-        let pb = PathBuf::from_str(&output_file_path).unwrap();
-        let output_file_parent_dir_path = pb.parent().unwrap();
-        debug!(
-            "Output file parent dir path: {:#?}",
-            &output_file_parent_dir_path
-        );
-        // if let Ok(_) = remove_dir_all(&output_file_parent_dir_path) {}
-        if let Ok(_) = create_dir_all(&output_file_parent_dir_path) {}
-        std::fs::copy(&file, &output_file_path).unwrap();
+        let out_file_path = Path::new(output_dir).join(file.strip_prefix(&cwd).unwrap());
+        create_dir_all(out_file_path.parent().unwrap()).ok();
+        std::fs::copy(file, out_file_path).unwrap();
     }
 }
 
 pub async fn render(files: &Vec<MarkDownFile>) {
-    let output_dir = PathBuf::from_str(".")
-        .unwrap()
-        .join("out")
-        .to_str()
-        .unwrap()
-        .to_string();
-    debug!("Rendering to {}", &output_dir);
-    remove_dir_all(&output_dir).unwrap();
-    create_dir_all(&output_dir).unwrap();
-
-    let associated_files = collect_associated_files(&files).await;
-    debug!("Associated files: {:#?}", &associated_files);
-    copy_associated_files(&associated_files, &output_dir);
-
+    // 1. Ensure output dir exists and it's fresh
+    let cwd = current_dir().unwrap();
+    let output_dir = Path::new(&cwd).join("output");
+    remove_dir_all(&output_dir).ok();
+    create_dir_all(&output_dir).ok();
+    // 2. Find all associated files(images, assets, etc) and copy them to output dir
+    //    Preserve the directory structure
+    copy_associated_files(&collect_associated_files(&files).await, &output_dir);
+    // 3. Render markdown files to html
+    //    Preserve the directory structure
     for file in files {
-        let html = to_html(&file.content).replace(".md", ".html");
-        let cwd = current_dir().unwrap();
-        let relative_file_path = file
-            .path
-            .strip_prefix(cwd.into_os_string().to_str().unwrap())
-            .unwrap()
-            .strip_suffix(".md")
-            .unwrap();
-        debug!("Relative file path: {:#?}", &relative_file_path);
-
-        let output_file_path = format!("{}{}.html", &output_dir, &relative_file_path);
-        debug!("Output file path: {:#?}", &output_file_path);
-
-        create_dir_all(
-            &PathBuf::from_str(&output_file_path)
-                .unwrap()
-                .parent()
-                .unwrap(),
-        )
-        .unwrap();
-        write(&output_file_path, &html).unwrap();
+        let html = to_html(&file.content)
+            // 4. Replace .md with .html in links
+            //    We are going to save them as .html
+            .replace(".md", ".html");
+        // 5. Calculate path to output file
+        //    cwd + output_dir + file path relative to cwd
+        //    Change ext from ".md" to ".html"
+        let mut out_file_path =
+            Path::new(&output_dir).join(Path::new(&file.path).strip_prefix(&cwd).unwrap());
+        out_file_path.set_extension("html");
+        // 6. Ensure dir tree exist and finally write the file
+        create_dir_all(&out_file_path.parent().unwrap()).ok();
+        write(&out_file_path, &html).unwrap();
     }
 }
